b402a0b57028a00e327d24a535906efe23b6de23
李思隆 <923631911@qq.com>
2016-07-20 15:22:48 +0800

    Add files via upload

--- choir/scripts/index.js
+++ choir/scripts/index.js
@@ -1,19 +1,39 @@
-﻿var width = $(window).width()//rem自适应
-$('html').css('fontSize', width / 127 +'px');
+﻿var width = $(window).width()/127//rem自适应
+$('html').css('fontSize', width +'px');
 
 window.onload=function(){
+	var documentHeight = $(document).height();//文本高度
+	var nav_pointer = $("#nav_pointer");
+	var scroll = $(window).scrollTop();
+	console.log(documentHeight);
+	if(scroll > ($("header").height()+$(".topImg").height())){
+			$("#nav").css("position","fixed");
+			$("#nav").css("top","0px");
+	}
+	
 	$(window).scroll(function(){
-		if($("body").scrollTop()>($("header").height()+$(".topImg").height())){
+		scroll = $(window).scrollTop();
+		if(scroll > ($("header").height()+$(".topImg").height())){
 			$("#nav").css("position","fixed");
 			$("#nav").css("top","0px");
 		}
 		else{
 			$("#nav").css("position","relative");
 		}
-	});
-	$(window).resize(function(){
-		var width = $(window).width()//rem自适应
-		$('html').css('fontSize', width / 127 +'px');
-	})
 
+		if (scroll>=0 && scroll<.325*documentHeight){
+			nav_pointer.css("margin-left","0");
+		}
+
+		else if (scroll>.325*documentHeight && scroll<.51*documentHeight){
+			nav_pointer.css("margin-left","25%");
+		}
+
+		else if(scroll>.51*documentHeight && scroll<.82*documentHeight){
+			nav_pointer.css("margin-left","50%");
+		}
+		else {
+			nav_pointer.css("margin-left","75%");
+		}	
+	});
 }
